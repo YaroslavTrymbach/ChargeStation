d5b0ae8a54ba62fbfe42a3563947a384d8077952
Yaroslav Trymbach <yaroslavtrymbach@gmail.com>
2018-07-12 19:01:31 +0300

    Serial control

--- Stm32/ChargeStationCentralCPU/Src/serial_control.c
+++ Stm32/ChargeStationCentralCPU/Src/serial_control.c
@@ -2,13 +2,17 @@
 #include "tasks.h"
 #include <cmsis_os.h>
 #include "usbd_cdc_if.h"
+#include "chargePointTime.h"
 
 const char* STR_OK = "OK";
 
 #define GET_BUF_SIZE 256
+#define MAX_COMMAND_LENGTH 32
 
 char getBuf[GET_BUF_SIZE];
 int getCnt = 0;
+char commandStr[MAX_COMMAND_LENGTH];
+char params[GET_BUF_SIZE - MAX_COMMAND_LENGTH];
 
 osThreadId serialControlTaskHandle;
 QueueHandle_t hMainQueue;
@@ -19,6 +23,36 @@ QueueHandle_t hSerialControlGetCharQueue = NULL;
 #define ERROR_STR_UNKNOWN_COMMAND    "Unknown command"
 #define ERROR_STR_INVALID_PARAMETERS "Invalid parameters"
 #define ERROR_STR_BUFFER_OVERFLOW    "Buffer overflow"
+
+#define COMMAND_TYPE_STANDARD 0
+#define COMMAND_TYPE_TEST     1   
+#define COMMAND_TYPE_READ     2
+#define COMMAND_TYPE_WRITE    3 
+#define COMMAND_TYPE_EXEC     4
+
+#define COMMAND_UNKNOWN 0
+
+#define COMMAND_EX_TIME 1
+
+void strupr(char *s){
+	int i, len;
+	len = strlen(s);
+	for(i = 0; i < len; i++){
+		if((s[i] >= 'a') && (s[i] <= 'z'))
+			s[i] -= 0x20;
+	}
+}
+
+int getCommandFromStr(char *comStr){
+	return COMMAND_UNKNOWN;
+}
+
+int getCommandExFromStr(char *comStr){
+	strupr(comStr);
+	if(strcmp(comStr, "TIME") == 0)
+		return COMMAND_EX_TIME;
+	return COMMAND_UNKNOWN;
+}
 
 
 void sendString(const char *s){
@@ -39,7 +73,30 @@ void sendHelloString(){
 	sendString(s);
 }
 
+void sendTime(){
+	char s[64];
+	struct tm t;
+	getCurrentTime(&t);
+	sprintf(s, "+TIME: %.4d-%.2d-%.2d %.2d:%.2d:%.2d", 
+	           t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec);
+	sendString(s);
+}
+
+void processCommandExRead(int command){
+	switch(command){
+		case COMMAND_EX_TIME:
+			sendTime();
+			break;
+	}
+	
+	sendOK();
+}
+
 void processLine(void){
+	int commandType = COMMAND_TYPE_STANDARD;
+	int commandLen;
+	int command;
+	char *p;
 	printf("Line is get: %s\n", getBuf);
 	
 	if(getCnt == 0){
@@ -47,6 +104,64 @@ void processLine(void){
 		sendOK();
 		return;
 	}
+	
+	if(getBuf[0] == '+'){
+		// It is extend command
+		// Get type of command
+		p = strstr(getBuf, "=?");
+		if(p != NULL)
+			commandType = COMMAND_TYPE_TEST;
+		else{
+			p = strstr(getBuf, "?");
+			if(p != NULL)
+				commandType = COMMAND_TYPE_READ;
+			else{
+				p = strstr(getBuf, "=");
+				if(p != NULL)
+					commandType = COMMAND_TYPE_WRITE;
+				else
+					commandType = COMMAND_TYPE_EXEC;
+			}			
+		}
+		
+		//Get command
+		if(commandType == COMMAND_TYPE_EXEC)
+			commandLen = strlen(getBuf);
+		else{
+			commandLen = (p - getBuf);
+		}
+		commandLen -= 1; //Start symbol '+' is needless
+		
+		if(commandLen >= MAX_COMMAND_LENGTH){
+			sendError(ERROR_STR_UNKNOWN_COMMAND);
+			return;
+		}
+		
+		strncpy(commandStr, getBuf + 1, commandLen);
+		commandStr[commandLen] = '\0';
+		
+		command = getCommandExFromStr(commandStr);
+		if(command == COMMAND_UNKNOWN){
+			sendError(ERROR_STR_UNKNOWN_COMMAND);
+			return;
+		}
+		
+		switch(commandType){
+			case COMMAND_TYPE_READ:
+				processCommandExRead(command);
+				break;
+		}
+	}
+	else{
+		//Standard AT command
+		/*command = getCommandFromStr(getBuf);
+		if(command == COMMAND_UNKNOWN){
+			sendError(ERROR_STR_UNKNOWN_COMMAND);
+			return;
+		}*/
+		sendError(ERROR_STR_UNKNOWN_COMMAND);
+	}
+	
 }
 
 #define STATE_WAIT_HEAD1 0  //Wait first symbol from start sequence "AT"
@@ -66,7 +181,7 @@ void serialControlThread(void const * argument){
 	while(true){
 		
 		if(xQueueReceive(hSerialControlGetCharQueue, &c, portMAX_DELAY) == pdPASS){
-			printf("SerialControl we got char %c\n", c);
+			//printf("SerialControl we got char %c\n", c);
 			switch(state){
 				case STATE_WAIT_HEAD1:
 					if((c == 'a') || (c == 'A')){
